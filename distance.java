fe434e0eae4e47cbedff88deb8baef30d50ae1cf
Utku Yurter <uyurter@na.edu>
2016-01-28 13:06:49 -0600

    Create distance.java

---
+++ distance.java
@@ -0,0 +1,28 @@
+// ************************************************************
+// Distance.java
+// Author: Utku Yurter
+// Computes the distance between two points
+// ************************************************************
+import java.util.Scanner;
+//import static java.lang.Math.pow;
+public class Distance
+{
+    public static void main (String[] args)
+    {
+        double x1, y1, x2, y2; // coordinates of two points
+        double distance; // distance between the points
+        Scanner scan = new Scanner(System.in);
+        // Read in the two points
+        System.out.print ("Enter the coordinates of the first point " +
+        "(put a space between them): ");
+        x1 = scan.nextDouble();
+        y1 = scan.nextDouble();
+        System.out.print ("Enter the coordinates of the second point: ");
+        x2 = scan.nextDouble();
+        y2 = scan.nextDouble();
+        // Compute the distance
+        distance = Math.sqrt((Math.pow((y1 - y2), 2)) + (Math.pow((x1 - x2), 2))) ;
+        // Print out the answer
+        System.out.println("Distance is: " + distance) ;
+    }
+}
