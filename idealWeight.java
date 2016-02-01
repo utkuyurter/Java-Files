191b9d85e8c6c5fc10cac39b36c8628bf6f0c392
utkuyurter <utkuyurter@outlook.com>
2016-01-31 22:49:05 -0600

    New Files 01/31/2016

--- idealWeight.java
+++ idealWeight.java
@@ -1,6 +1,8 @@
 /*
  * Author: Utku Yurter      Program Name: idealWeight.java
- * Program Description: This program calculates the ideal weight for males and females.
+ *
+ * Program Description: This program calculates the ideal weight and range for males and females 
+ * depending on their height.
  */
 package idealweight;
 
@@ -15,8 +17,7 @@ public class IdealWeight
       int heightFeet;
       int heightInches;
       int height ;
-      int idealWeightMale ;
-      int idealWeightFemale ;
+      int idealWeight ;
       int rangeLowest ;
       int rangeHighest ;
       
@@ -27,21 +28,21 @@ public class IdealWeight
       
       height = (heightFeet * 12) + heightInches ;
       
-      idealWeightMale = 106 + ((height - 60) * 6) ;
+      idealWeight = 106 + ((height - 60) * 6) ;
       
-      rangeHighest = (int) ((idealWeightMale * 0.15) + idealWeightMale) ;
-      rangeLowest = (int) (idealWeightMale - (idealWeightMale * 0.15)) ;
+      rangeHighest = (int) ((idealWeight * 0.15) + idealWeight) ;
+      rangeLowest = (int) (idealWeight - (idealWeight * 0.15)) ;
          
-      System.out.println("Your ideal weight is " + idealWeightMale + " if you are a male.");  
+      System.out.println("Your ideal weight is " + idealWeight + " if you are a male.");  
       System.out.println("The ideal weight range is " + rangeLowest + "---" + rangeHighest) ;
              
-      idealWeightFemale = 100 + ((height - 60) * 5) ;
+      idealWeight = 100 + ((height - 60) * 5) ;
       
-      rangeHighest = (int) ((idealWeightFemale * 0.15) + idealWeightFemale) ;
-      rangeLowest = (int) (idealWeightFemale - (idealWeightFemale * 0.15)) ;
+      rangeHighest = (int) ((idealWeight * 0.15) + idealWeight) ;
+      rangeLowest = (int) (idealWeight - (idealWeight * 0.15)) ;
               
-      System.out.println("Your ideal weight is " + idealWeightFemale + " if you are a female."); 
+      System.out.println("Your ideal weight is " + idealWeight + " if you are a female."); 
       System.out.println("The ideal weight range is " + rangeLowest + "---" + rangeHighest) ;
         
-    }
+    }    
 }
